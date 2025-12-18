8110d9a9786259f5ab9cc2fce210c423f500b9c9
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-12-18 14:17:43 -0300

    update

--- l2/widget/inputNumber/index.ts
+++
@@ -1,9 +0,0 @@
-/// <mls shortName="index" project="102026" enhancement="_blank" folder="widget/inputNumber" />
-
-export const widgetDefinition = {
-    name: "numberInput",
-    description: "Campo para inserir um número",
-    properties: [
-        { name: "value", type: "number" }
-    ]
-}
