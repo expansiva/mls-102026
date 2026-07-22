308e8aee64d8c16bb9b8583b3a448b232d1c6492
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-07-22 15:22:40 -0300

    Update designSystem.defs.ts

--- l2/designSystem.defs.ts
+++ l2/designSystem.defs.ts
@@ -1,2 +1,2 @@
-/// <mls fileReference="_102026_/l2/designSystem.defs.ts" enhancement="_102027_/l2/enhancementLit" />
+/// <mls fileReference="_102026_/l2/designSystem.defs.ts" enhancement="_102027_/l2/enhancementLit" /> 
 
